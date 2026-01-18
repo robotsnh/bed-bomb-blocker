72a521c7a60b8fa544a0906e7aa11ac120506a69
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-01-18 17:58:19 +0000

    __now__ will it work?

--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
@@ -11,7 +11,7 @@ public class Bedbombblocker implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID)
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
