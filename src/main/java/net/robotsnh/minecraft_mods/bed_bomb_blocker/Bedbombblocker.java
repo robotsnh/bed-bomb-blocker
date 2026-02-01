e0038ee2f544e234ed62203fffc560458cc33ff2
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-02-01 14:25:54 +0000

    Cleaned up some comments

--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
@@ -8,9 +8,7 @@ import org.slf4j.LoggerFactory;
 public class Bedbombblocker implements ModInitializer {
 	public static final String MOD_ID = "bed-bomb-blocker";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	// The logger is used to write text to the console and the log file.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
