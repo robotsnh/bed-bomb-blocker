5045a65b7d4f75c33ec31bbf344c553dfff40024
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-03-01 13:34:29 +0000

    Removed an extra line in boilerplate for the main file

--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
@@ -14,7 +14,6 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 */
 
 package net.robotsnh.minecraft_mods.bed_bomb_blocker;
