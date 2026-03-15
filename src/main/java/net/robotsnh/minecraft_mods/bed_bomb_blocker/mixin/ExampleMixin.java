74efea25856ac792e53022a0b30076b56035785a
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-03-15 17:35:21 +0000

    Changed GPL to AGPL in SPDX identifier and added SPDX identifier to `gradle/wrapper/gradle-wrapper.properties`

--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/mixin/ExampleMixin.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (c) robotsnh and project contributors
 
 package net.robotsnh.minecraft_mods.bed_bomb_blocker.mixin;
