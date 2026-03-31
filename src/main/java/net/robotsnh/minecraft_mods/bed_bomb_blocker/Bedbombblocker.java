f3e07a24cd9828f408b85ae6afb79ca5b07f3df6
robotsnh <88983536+robotsnh@users.noreply.github.com>
2026-03-31 05:55:53 -0700

    Created docstrings for all functions

--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/Bedbombblocker.java
@@ -25,6 +25,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Bedbombblocker implements ModInitializer {
+
+    /*
+    The entrypoint class of our mod, which aims to stop beds from exploding in 
+    the Nether/End via code injection into the block interaction method/s.
+    */
+
 	public static final String MOD_ID = "bed-bomb-blocker";
 
 	// The logger is used to write text to the console and the log file.
@@ -32,6 +38,11 @@ public class Bedbombblocker implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-        
+
+        /*
+        An initialization method for our mod. This should print a simple logger
+        message.
+        */
+
 	}
 }
--- src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/mixin/BedBlockMixin.java
+++ src/main/java/net/robotsnh/minecraft_mods/bed_bomb_blocker/mixin/BedBlockMixin.java
@@ -12,12 +12,24 @@ import org.slf4j.LoggerFactory;
 
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
+
+	/*
+	Our mod's main logic class. It intercepts a block interaction event to stop
+	the interaction if the block is a bed and the bed is in the Nether or End.
+	*/
+
 	public static Logger LOGGER = LoggerFactory.getLogger("bed-bomb-blocker");
 
 	@Inject(at = @At("HEAD"),
 			method = "onUse",
 			cancellable = true)
 	private void init(CallbackInfoReturnable<InteractionResult> cir) {
+
+		/*
+		Our main code injection function. It intercepts a block interaction
+		function call, storing it in the local variable `cir`.
+		*/
+
 		LOGGER.info("Bed was interacted with!");
 	}
 }
